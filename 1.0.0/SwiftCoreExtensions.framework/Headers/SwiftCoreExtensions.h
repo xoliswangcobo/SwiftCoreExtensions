06a5e55d81c3e9b2156d7d94eec2f21903c90b84
Xoliswa Ngcobo <xoliswangcobo@icloud.com>
2019-10-30 13:52:18 +0200

    Version 1.0.0

---
+++ 1.0.0/SwiftCoreExtensions.framework/Headers/SwiftCoreExtensions.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCoreExtensions.h
+//  SwiftCoreExtensions
+//
+//  Created by Xoliswa on 2019/10/24.
+//  Copyright © 2019 AppRoot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftCoreExtensions.
+FOUNDATION_EXPORT double SwiftCoreExtensionsVersionNumber;
+
+//! Project version string for SwiftCoreExtensions.
+FOUNDATION_EXPORT const unsigned char SwiftCoreExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCoreExtensions/PublicHeader.h>
+
+
